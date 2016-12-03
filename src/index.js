6574b8e6b61857be53b429e024172b40438c0409
Sjaak Luthart <mail@sjaakluthart.nl>
2016-12-03 14:13:03 +0100

    Added css setup.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import './css/reset.css';
+import './css/fonts.css';
+import './css/index.css';
 
 ReactDOM.render(
   <App />,
